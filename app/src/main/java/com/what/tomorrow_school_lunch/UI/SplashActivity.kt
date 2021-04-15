47bbfdf976accdb23ab55908f5532c21be3ddc38
ParkSangSun1 <appleasz1028102@gmail.com>
2021-04-15 23:31:36 +0900

    [CREATE]Splash화면

---
+++ app/src/main/java/com/what/tomorrow_school_lunch/UI/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.what.tomorrow_school_lunch.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.what.tomorrow_school_lunch.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
